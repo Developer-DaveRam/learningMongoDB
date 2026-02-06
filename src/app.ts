09672868a05942ef83a478fbe1d0982bf536f40b
Developer-Daveram <daveramskyraan@gmail.com>
2026-02-06 12:44:30 +0530

    folder structure completed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { db } from './db/db.js'
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+db()
 
 app.listen(PORT,()=>{
     console.log(`Server is on port http://localhost:${PORT}`)
